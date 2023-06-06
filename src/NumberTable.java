d16ac76d8bbe058aec5e1c8d927bfa4ff3dc046b
Vodiik <martinvoda122@gmail.com>
2023-06-06 09:57:18 +0200

    second

--- src/NumberTable.java
+++ src/NumberTable.java
@@ -6,15 +6,83 @@ import java.util.ArrayList;
 public class NumberTable extends JTable {
 
     private ArrayList<Integer> arrayList = new ArrayList<>();
+    private String jsonString;
 
     public NumberTable() {
         init();
     }
 
+    public void addToList(String value) {
+        System.out.println("přidáno do listu: " + value);
+
+        jsonString = value;
+
+        // Remove leading and trailing square brackets from the JSON string
+        value = value.substring(1, value.length() - 1);
+
+        // Split the JSON string into individual objects
+        String[] objects = value.split("\\},\\{");
+
+        for (String object : objects) {
+            // Remove curly braces from the object string
+            object = object.replace("{", "").replace("}", "");
+
+            // Split the object string into key-value pairs
+            String[] keyValuePairs = object.split(",");
+
+            int a = 0, b = 0, vysl = 0;
+            String oper = "";
+
+            for (String keyValuePair : keyValuePairs) {
+                // Split the key-value pair into key and value
+                String[] parts = keyValuePair.split(":");
+
+                String key = parts[0].trim();
+                String valueStr = parts[1].trim();
+
+                // Remove quotes from the value string
+                valueStr = valueStr.replace("\"", "");
+
+                if (key.equals("a")) {
+                    a = Integer.parseInt(valueStr);
+                } else if (key.equals("oper")) {
+                    oper = valueStr;
+                } else if (key.equals("b")) {
+                    b = Integer.parseInt(valueStr);
+                } else if (key.equals("vysl")) {
+                    vysl = Integer.parseInt(valueStr);
+                }
+            }
+
+            int result = calculateResult(a, oper, b);
+            this.arrayList.add(result);
+        }
+
+        updateModel();
+    }
+
+    private int calculateResult(int a, String oper, int b) {
+        if (oper.equals("+")) {
+            return a + b;
+        } else if (oper.equals("-")) {
+            return a - b;
+        } else if (oper.equals("*")) {
+            return a * b;
+        } else if (oper.equals("/")) {
+            return a / b;
+        } else {
+            if (oper.isEmpty()) {
+                return 0;
+            } else {
+                throw new IllegalArgumentException("Invalid operator: " + oper);
+            }
+        }
+    }
+
     private void init() {
         setBackground(Color.red);
 
-        String[] columnNames = {"Number 1", "Number 2", "Result"};
+        String[] columnNames = {"Number 1", "Operator", "Number 2", "Result"};
 
         Object[][] data = new Object[0][columnNames.length];
 
@@ -28,7 +96,34 @@ public class NumberTable extends JTable {
         model.setRowCount(0);
 
         for (Integer item : arrayList) {
-            model.addRow(new Object[]{item, "", ""});
+            model.addRow(new Object[]{item, null, null, null});
+        }
+
+        int rowIndex = 0;
+        for (Integer result : arrayList) {
+            int startIndex = jsonString.indexOf("{", rowIndex);
+            int endIndex = jsonString.indexOf("}", startIndex);
+
+            String object = jsonString.substring(startIndex + 1, endIndex);
+            String[] keyValuePairs = object.split(",");
+
+            for (String keyValuePair : keyValuePairs) {
+                String[] parts = keyValuePair.split(":");
+                String key = parts[0].replace("\"", "").trim();
+                String valueStr = parts[1].replace("\"", "").trim();
+
+                if (key.equals("a")) {
+                    model.setValueAt(Integer.parseInt(valueStr), rowIndex, 0);
+                } else if (key.equals("oper")) {
+                    model.setValueAt(valueStr, rowIndex, 1);
+                } else if (key.equals("b")) {
+                    model.setValueAt(Integer.parseInt(valueStr), rowIndex, 2);
+                } else if (key.equals("vysl")) {
+                    model.setValueAt(Integer.parseInt(valueStr), rowIndex, 3);
+                }
+            }
+
+            rowIndex++;
         }
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -2,13 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JFrame {
-
     static NumberTable numberTable = new NumberTable();
 
     public Window(int width, int height) throws HeadlessException {
         init(width, height);
 
     }
+
+    //String initValue = "[]";
+    String infoLabelValue = "Vložte pole JSON do textfieldu";
 
     private void init(int width, int height) {
         setTitle("Zkouška");
@@ -20,8 +22,24 @@ public class Window extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
+        JLabel infoLabel = new JLabel(infoLabelValue);
+        panel.add(infoLabel);
+        JButton button = new JButton("Load");
+        panel.add(button);
+
+        JTextField textField = new JTextField();
+        textField.setMaximumSize(
+                new Dimension(width, 200));
+        panel.add(textField);
+
         panel.add(new JScrollPane(numberTable));
-        
+
+        button.addActionListener(e -> {
+            System.out.println(textField.getText());
+            numberTable.addToList(textField.getText());
+        });
+
+
         add(panel);
 
 
