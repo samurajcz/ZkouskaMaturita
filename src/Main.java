2cc55e0b9caceb64a8a64d65b0e6021014f18ffa
buresvl2 <vlastabures13@gmail.com>
2023-06-06 09:04:57 +0200

    yes

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    // Vytvořte aplikaci s GUI, která přečte do seznamu jednoduché početní příklady na sčítání z níže uvedeného souboru.
+    // Některé z příkladů mají výsledky spočtené správně, některé jsou špatně. Vaším úkolem je zobrazit pomocí vhodných komponent v GUI příklady s chybným výsledkem a celkovou statistiku
+    // - počet příkladů celkem, počet správných a počet špatných výsledků.
+
+    // [{"a":28,"oper":"+","b":68,"vysl":92},{"a":59,"oper":"+","b":85,"vysl":144},{"a":2,"oper":"+","b":100,"vysl":102},{"a":12,"oper":"+","b":72,"vysl":86},{"a":72,"oper":"+","b":95,"vysl":167},{"a":17,"oper":"+","b":78,"vysl":95},{"a":19,"oper":"+","b":17,"vysl":36},{"a":88,"oper":"+","b":39,"vysl":123},{"a":25,"oper":"+","b":67,"vysl":92},{"a":74,"oper":"+","b":32,"vysl":106},{"a":19,"oper":"+","b":85,"vysl":104},{"a":46,"oper":"+","b":84,"vysl":131},{"a":32,"oper":"+","b":63,"vysl":95},{"a":19,"oper":"+","b":9,"vysl":28},{"a":33,"oper":"+","b":59,"vysl":92},{"a":65,"oper":"+","b":55,"vysl":120},{"a":33,"oper":"+","b":93,"vysl":126},{"a":9,"oper":"+","b":73,"vysl":80},{"a":94,"oper":"+","b":98,"vysl":196},{"a":79,"oper":"+","b":75,"vysl":154}]
+
+
 }
